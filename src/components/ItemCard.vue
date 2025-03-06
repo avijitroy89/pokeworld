aa5ad01e6d5876c247a82cec5873c5e87625fd2b
unknown <avijitroy18@gmail.com>
2025-03-06 17:34:58 +0530

    created card component for each pokemon details and added card  design with bootstrap

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted } from 'vue';
+
+
+onMounted(async () => {
+})
+
+</script>
+
+<template>
+    <div class="card">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/1.png"
+            class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">name</h5>
+            <p class="card-text">height: <span>23</span></p>
+            <p class="card-text">weight: <span>30</span></p>
+        </div>
+        <div class="card-header">
+            List of Abilities
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ability 1</li>
+            <li class="list-group-item">ability 2</li>
+        </ul>
+    </div>
+
+</template>
+
+<style></style>
